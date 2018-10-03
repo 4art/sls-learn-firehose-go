322499f2e94d3f2892b9a498d1a199d5c3cb17e0
metraf <firsov.tyoma@gmail.com>
2018-10-03 15:17:44 +0200

    added methods to s3 service

--- hello/service/s3Service.go
+++ hello/service/s3Service.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"../model"
+	"encoding/json"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
@@ -28,6 +30,65 @@ func IsBucketExist(bucket string) bool {
 		}
 	}
 	return false
+}
+
+func IsBucketEmpty(bucket string) bool {
+	return len(getListObjectsOutput(bucket).Contents) == 0
+}
+
+func ClearBucket(bucket string) {
+	for _, v := range getListObjectsOutput(bucket).Contents {
+		svc.DeleteObject(&s3.DeleteObjectInput{Key: v.Key})
+	}
+}
+
+func PullObjectsFromS3(bucket string) []string {
+	var objects []string
+	for _, v := range getListObjectsOutput(bucket).Contents {
+		objects = append(objects, *v.Key)
+	}
+	return objects
+}
+
+func PullCities(bucket string, key string) []model.City {
+	var cities []model.City
+	jsonStr := QueryJson(bucket, key, "SELECT * FROM s3Object")
+	err := json.Unmarshal([]byte(jsonStr), &cities)
+	if err != nil {
+		panic(err)
+	}
+	return cities
+}
+
+func QueryJson(bucket string, key string, query string) string {
+	compressionType := "GZIP"
+	jsonType := "LINES"
+	recordDelimiter := "\n"
+	selectObjectContentOutput, e := svc.SelectObjectContent(&s3.SelectObjectContentInput{
+		Bucket:     &bucket,
+		Key:        &key,
+		Expression: &query,
+		InputSerialization: &s3.InputSerialization{
+			CompressionType: &compressionType,
+			JSON:            &s3.JSONInput{Type: &jsonType},
+		},
+		OutputSerialization: &s3.OutputSerialization{
+			JSON: &s3.JSONOutput{RecordDelimiter: &recordDelimiter},
+		},
+	})
+	if e != nil {
+		panic(e)
+	}
+	return selectObjectContentOutput.GoString()
+}
+
+func getListObjectsOutput(bucket string) *s3.ListObjectsOutput {
+	listObjectsInput := s3.ListObjectsInput{Bucket: &bucket}
+	listObjectsOutput, e := svc.ListObjects(&listObjectsInput)
+	if e != nil {
+		panic(e)
+	}
+	return listObjectsOutput
 }
 
 func createService() *s3.S3 {
